7b35d4b5aa1854d0d86f0f26f74137ac6c20b157
unknown <tfauvelle@gmail.com>
2021-08-15 15:55:48 -0500

    added new class.

--- src/com/careerDevs/Albums.java
+++ src/com/careerDevs/Albums.java
@@ -1,5 +1,17 @@
 package com.careerDevs;
 
 public class Albums {
+    public String artistName;
+    public String albumName;
+    public String condition;
+    public double playBackSpeed;
+
+    public Albums(String artistName, String albumName, String condition, double playBackSpeed) {
+        this.artistName = artistName;
+        this.albumName = albumName;
+        this.condition = condition;
+        this.playBackSpeed = playBackSpeed;
+
+    }
 
 }
--- src/com/careerDevs/WantList.java
+++ src/com/careerDevs/WantList.java
@@ -1,4 +1,12 @@
 package com.careerDevs;
 
-public class WantList {
+public class WantList extends Albums {
+    public String rarity;
+    public double currentMarketPrice;
+
+    public WantList(String artistName, String albumName, String condition, double playBackSpeed, String rarity, double currentMarketPrice) {
+        super(artistName, albumName, condition,playBackSpeed);
+        this.rarity = rarity;
+        this.currentMarketPrice = currentMarketPrice;
+    }
 }
